fa538265914f45450dff71ac1cf178e999d4c944
remon551 <65865953+remon551@users.noreply.github.com>
2023-04-13 18:37:22 +0200

    p3(not done yet)

---
+++ p3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"stack.cpp"
+using namespace std;
+int main()
+{
+    string infix, postfix = "";
+    getline(cin, infix);
+
+}
--- stack.cpp
+++ stack.cpp
@@ -96,27 +96,3 @@ class stack
             }
         }
 };
-int main()
-{
-    stack<int> s;
-    s.push(0);
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.push(4);
-    s.push(5);
-    s.push(6);
-    while(!s.isEmpty())
-    {
-        cout << s.pop() << '\n';
-        s.print();
-    }
-    s.push(0);
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.push(4);
-    s.push(5);
-    s.push(6);
-    s.print();
-}
